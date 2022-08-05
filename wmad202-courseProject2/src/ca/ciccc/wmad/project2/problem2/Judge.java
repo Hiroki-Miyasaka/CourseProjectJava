027f09862dccdc0e3f31a84cd022330698a8440e
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-08-04 19:46:07 -0700

    modified problem2

--- wmad202-courseProject2/src/ca/ciccc/wmad/project2/problem2/Judge.java
+++ wmad202-courseProject2/src/ca/ciccc/wmad/project2/problem2/Judge.java
@@ -1,9 +1,26 @@
 package ca.ciccc.wmad.project2.problem2;
 
+import java.util.ArrayList;
+
 public class Judge {
 
-    private Integer numberOfBattles;
-    private String theWinningTeam;
-    private String theSurvivingMembersOfTheLoosingTeam;
+    private ArrayList<Transformer> autobots;
+    private ArrayList<Transformer> deceptions;
+
+    public Judge(ArrayList<Transformer> autobots, ArrayList<Transformer> deceptions) {
+        this.autobots = autobots;
+        this.deceptions = deceptions;
+    }
+
+    public int getTheNumberOfBattles(){
+        if(autobots.size() >= deceptions.size()){
+            return deceptions.size();
+        }
+        return autobots.size();
+    }
+
+    public String goToBattle(){
+
+    }
 
 }
